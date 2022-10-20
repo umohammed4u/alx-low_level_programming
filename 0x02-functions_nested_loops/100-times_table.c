c6742da7ba15e0edb7bfc3cfc1493e6562021fdc
umohammed4u <umohammed4u@gmail.com>
2022-10-20 04:05:42 -0700

    task100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,10 +17,11 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
-				if (j == 0)
 
+				if (j == 0)
 				{
 					_putchar(k + '0');
+
 				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
@@ -28,6 +29,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
+
 				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
@@ -35,6 +37,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
+
 				} else if (k >= 100)
 				{
 					_putchar(',');
