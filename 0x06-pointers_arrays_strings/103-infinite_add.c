b16dec020ed69ea4d5526aa7a812bf5c5300db91
umohammed4u <umohammed4u@gmail.com>
2022-10-27 14:40:03 -0700

    task104

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,16 +7,12 @@
  */
 
 void rev_string(char *n)
-
 {
 	int i = 0;
-
 	int j = 0;
-
 	char temp;
 
 	while (*(n + i) != '\0')
-
 	{
 		i++;
 	}
@@ -24,14 +20,12 @@ void rev_string(char *n)
 	i--;
 
 	for (j = 0; j < i; j++, i--)
-
 	{
 		temp = *(n + j);
 		*(n + j) = *(n + i);
 		*(n + i) = temp;
 	}
 }
-
 /**
  * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
@@ -44,7 +38,6 @@ void rev_string(char *n)
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
-
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
@@ -55,7 +48,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 		j++;
 	i--;
-
 	j--;
 
 	if (j >= size_r || i >= size_r)
@@ -63,7 +55,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		return (0);
 
 	while (j >= 0 || i >= 0 || overflow == 1)
-
 	{
 		if (i < 0)
 
@@ -88,20 +79,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (digits >= (size_r - 1))
 
 			return (0);
-
 		*(r + digits) = (temp_tot % 10) + '0';
-
 		digits++;
-
 		j--;
 		i--;
 	}
 	if (digits == size_r)
 		return (0);
-
 	*(r + digits) = '\0';
-
 	rev_string(r);
-
 	return (r);
 }
