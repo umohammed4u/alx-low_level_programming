5b9d879f38a4ddfb0df360a6c3780a29c02e12f2
umohammed4u <umohammed4u@gmail.com>
2022-11-05 07:16:32 -0700

    libmy

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - printing out a string.
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	_puts("\"At the end, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+
+	return (0);
+
+}
