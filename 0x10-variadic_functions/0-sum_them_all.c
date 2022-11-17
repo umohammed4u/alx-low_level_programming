9e5c4e7f1fd76ea00df80413404cd1f6207b06d6
umohammed4u <umohammed4u@gmail.com>
2022-11-17 14:12:24 -0800

    sumthemall

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,5 @@
 #include "variadic_functions.h"
 
-
 /**
  * sum_them_all - calculates the sum of all its parameters
  * @n: number of arguments passed to the function
@@ -26,3 +25,5 @@ int sum_them_all(const unsigned int n, ...)
 
 	return (sum);
 }
+
+
